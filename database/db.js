a553aa4ab033e3ea1b667f8b85201570524c8cfe
Matheus Bickel <devbickel@gmai.com>
2022-02-11 12:16:33 -0300

    Altera server para app e ajusta config de banco

--- database/db.js
+++ database/db.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql');
-const dotenv = require('dotenv');
-dotenv.config({path: './.env'});
+const dotenv = require('dotenv').config({ path: '../.env'});
 
-const database = mysql.createConnection ({})
+const database = mysql.createConnection ({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
 
-database.connect(function(err) {
-    if (err) throw err;
+database.connect( function (error) {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log('Connected')
+    }
 });
 
 module.exports = database;
