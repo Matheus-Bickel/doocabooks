4c901972cdd3fbe1f0b3b9f219a33dd89bd83f55
Matheus Bickel <devbickel@gmai.com>
2022-02-12 12:56:54 -0300

    adiciona model books e retorna para route a consulta em banco

--- app/data/booksData.js
+++ app/data/booksData.js
@@ -4,3 +4,8 @@ exports.getBooks = async function () {
 
     return await database.query('select * from books')
 }
+
+exports.getOneBook = async function (id) {
+
+    return await database.query('select * from books where id = ${id}')
+}
